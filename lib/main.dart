74c77031f4ee691c03a5bf9118cd1f8ddadc01e5
SYEDawais77 <Shah.syedawais77@yahoo.com>
2022-10-16 18:01:19 +0500

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        )
+      ),
+      home: const RandomWords(),
     );
   }
 }
@@ -32,6 +38,7 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
         appBar: AppBar(
           title: const Text("Startup Name Generator"),
           actions: [
